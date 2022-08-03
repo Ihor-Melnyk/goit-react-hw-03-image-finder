1d4d26120ee9daf206b01b489b369c4a30b35c0f
Ihor Melnyk <92684157+Ihor-Melnyk@users.noreply.github.com>
2022-08-03 15:54:57 +0300

    finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,23 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar';
+import ImageInfo from './ImageInfo';
+import { Toaster } from 'react-hot-toast';
+
+export class App extends Component {
+  state = {
+    imageName: '',
+  };
+  handleFormSubmit = imageName => {
+    this.setState({ imageName });
+  };
+
+  render() {
+    return (
+      <div className={'App'}>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageInfo imageName={this.state.imageName} />
+        <Toaster position="top-left" reverseOrder={false} />
+      </div>
+    );
+  }
+}
